4009b95ab7c94859c09fe66595d32a7d521f4649
dzarief <dzarief@telcowin.com>
2022-03-23 16:56:10 +0800

    testing

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
+app = require("./publica/");
+app.use("/", main); 
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('index.html')
+    //res.sendFile(__dirname + '/public/index.html');
     });
 
 // setup http server to listen on HTTP_PORT
