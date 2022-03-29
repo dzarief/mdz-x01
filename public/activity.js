8d099c9176280c4d08efff9261e789b5a33dd742
dzarief <dzarief@telcowin.com>
2022-03-29 13:04:20 +0800

    testing

--- public/activity.js
+++ public/activity.js
@@ -4,11 +4,12 @@ var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity', function( data ) {
+  console.log('toll');
   document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
 });
 
 // Save Sequence
 connection.on('clickedNext', function() {
-  //var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
-  //connection.trigger('updateActivity', configuration);
+  var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
+  connection.trigger('updateActivity', configuration);
 });
