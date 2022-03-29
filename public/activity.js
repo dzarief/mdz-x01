feacea6d586da4a0ef15ee9cf28d38e5b7419490
dzarief <dzarief@telcowin.com>
2022-03-29 14:14:32 +0800

    testing

--- public/activity.js
+++ public/activity.js
@@ -4,7 +4,8 @@ var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity', function( data ) {
-  console.log('toll');
+  console.log('toll'+jsonData);
+  
   document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
 });
 
