f1a9ddd30f304ddd34630a10a98ea74e873b2f2d
dzarief <dzarief@telcowin.com>
2022-04-14 18:24:03 +0800

    testing

--- public/activity.js
+++ public/activity.js
@@ -5,12 +5,33 @@ connection.trigger('ready');
 
 connection.on('initActivity', function( data ) {
   document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
-  configuration["arguments"].execute.inArguments.Name = 'Hello x';
+  
 });
 
 // Save Sequence
 connection.on('clickedNext', function() {
   var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
-  configuration["arguments"].execute.inArguments.Name = 'Hello';
+
+  var message;
+  var hasInArguments = Boolean(
+    configuration["arguments"] &&
+    configuration["arguments"].execute &&
+      configuration["arguments"].execute.inArguments &&
+      configuration["arguments"].execute.inArguments.length > 0
+  );
+
+  var inArguments = hasInArguments
+    ? configuration["arguments"].execute.inArguments
+    : {};
+
+  $.each(inArguments, function (index, inArgument) {
+    $.each(inArgument, function (key, val) {
+      if (key === "message") {
+        val = "hello";
+      }
+    });
+  });
+
+  
   connection.trigger('updateActivity', configuration);
 });
