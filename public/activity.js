bc085362c8d43064cc3ab0bf4215ecb6662f8390
dzarief <dzarief@telcowin.com>
2022-04-15 09:56:20 +0800

    testing

--- public/activity.js
+++ public/activity.js
@@ -2,16 +2,19 @@ var connection = new Postmonger.Session();
 
 // Startup Sequence
 connection.trigger('ready');
+var payload = {};
 
 connection.on('initActivity', function( data ) {
-  
+  if (data) {
+    payload = data;
+  }
   document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
   
 });
 
 // Save Sequence
 connection.on('clickedNext', function() {
-  var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
+  //var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
   
-  connection.trigger('updateActivity', configuration);
+  connection.trigger('updateActivity', payload);
 });
